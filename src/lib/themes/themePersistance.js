aa8b7fb938401d1b3bd144fd598b7f3dc69b7405
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-03-09 21:19:01 -0500

    dark theme real

--- src/lib/themes/themePersistance.js
+++ src/lib/themes/themePersistance.js
@@ -6,7 +6,7 @@ const PREFERS_HIGH_CONTRAST_QUERY = '(prefers-contrast: more)';
 const COOKIE_KEY = 'scratchtheme';
 
 // Dark mode isn't enabled yet
-const isValidTheme = theme => [DEFAULT_THEME, HIGH_CONTRAST_THEME].includes(theme);
+const isValidTheme = theme => [DEFAULT_THEME, HIGH_CONTRAST_THEME, DARK_THEME].includes(theme);
 
 const systemPreferencesTheme = () => {
     if (window.matchMedia && window.matchMedia(PREFERS_HIGH_CONTRAST_QUERY).matches) return HIGH_CONTRAST_THEME;
